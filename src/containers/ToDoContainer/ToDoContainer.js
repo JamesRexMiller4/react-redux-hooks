465268a26239fac00c4db85a030abe9bdea80838
James Miller <james.rex.miller.4@gmail.com>
2020-01-02 22:56:20 -0700

    Add conditional rendering for Completed component

--- src/containers/ToDoContainer/ToDoContainer.js
+++ src/containers/ToDoContainer/ToDoContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ToDoItem from '../ToDoItem/ToDoItem';
+import Completed from '../../components/Completed/Completed';
 import { connect } from 'react-redux';
 import './ToDoContainer.scss';
 
 const ToDoContainer = ({todos, removeTodo}) => {
-  const todoList = todos.map(todo => {
+  let todoList;
+
+  if (todos.length < 1) {
+    todoList = <Completed />
+  } else {
+    todoList = todos.map(todo => {
     return <ToDoItem 
       text={todo.title} 
       key={todo.id}
@@ -12,6 +18,7 @@ const ToDoContainer = ({todos, removeTodo}) => {
       removeTodo={removeTodo}
     />
   })
+}
   
   return (
     <div className="todo-container">
