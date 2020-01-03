5d848eee7b818ae019597521b3331a4f3b38a52f
James Miller <james.rex.miller.4@gmail.com>
2020-01-02 22:58:33 -0700

    Add Completed component files

--- src/components/ToDoContainer/ToDoContainer.js
+++ src/components/ToDoContainer/ToDoContainer.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ToDoItem from '../ToDoItem/ToDoItem';
+import Completed from '../Completed/Completed';
 import './ToDoContainer.scss';
 
 const ToDoContainer = ({todos, removeTodo}) => {
-  const todoList = todos.map(todo => {
-    return <ToDoItem 
-      text={todo.title} 
-      key={todo.id}
-      id={todo.id}
-      removeTodo={removeTodo}
-    />
-  })
+  let todoList;
+
+  if (todos.length < 1) {
+    todoList = <Completed />
+  } else {
+      todoList = todos.map(todo => {
+        return <ToDoItem 
+          text={todo.title} 
+          key={todo.id}
+          id={todo.id}
+          removeTodo={removeTodo}
+        />
+      })
+  }
   return (
     <div className="todo-container">
       {todoList}
