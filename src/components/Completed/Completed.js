f8d29abe60472cab2ae2d34da429b68a41a067e4
James Miller <james.rex.miller.4@gmail.com>
2020-01-02 23:00:27 -0700

    Add functionality to Completed component

---
+++ src/components/Completed/Completed.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Completed.scss'
+;
+const Completed = () => {
+  return (
+    <>
+    <div className='completed'>
+    </div>
+    <h2 className='completed-h2'>Make more ToDos!</h2>
+    </>
+  )
+}
+export default Completed;
